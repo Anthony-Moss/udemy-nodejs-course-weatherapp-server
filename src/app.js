f8d350297a42e60865142ebbedd47d7c194bc73f
Anthony Moss <asmoss93@gmail.com>
2019-08-08 22:48:36 -0400

    removes the html files and replaces with hbs files for dynamic support

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ const express = require('express')
 
 const app = express()
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../views')
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static diectory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res)  => {
@@ -18,7 +24,15 @@ app.get('', (req, res)  => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: "About Page"
+        title: "About Site",
+        name: 'Anthony Moss'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: "Enter your city at /weather to get local weather info"
     })
 })
 
