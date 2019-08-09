10999f699a681a35dce8b7483b3d9fed1f49fae5
Anthony Moss <asmoss93@gmail.com>
2019-08-08 22:00:51 -0400

    brings in code for the webserver for weatherapp, changed organization of project files

---
+++ src/app.js
@@ -0,0 +1,28 @@
+
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res)  => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Anthony Moss'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Sunny',
+        location: 'Atlanta'
+    })
+})
+
+app.listen(3001, () => {
+    console.log(`Server is running on port 3001`)
+})
