828ae42c8ccc6affb3d090950abe4cbd38f58b4f
Anthony Moss <asmoss93@gmail.com>
2019-08-08 23:11:26 -0400

    adds header which is page title and simple nav bar

--- src/app.js
+++ src/app.js
@@ -35,7 +35,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
-        message: "Enter your city at /weather to get local weather info"
+        message: "Enter your city at /weather to get local weather info",
+        name: 'Anthony Moss'
     })
 });
 
