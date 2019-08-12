438afca6d21aafa24f219b9be06bb12c7c77e5fa
Anthony Moss <asmoss93@gmail.com>
2019-08-12 18:18:12 -0400

    adds browser http request using fetch to get weather info

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side js file is loaded!')
+
+
+fetch('http://localhost:3001/weather?address=atlanta').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.forecast)
+            console.log(data.location)
+        }
+    })
+})
