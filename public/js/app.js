66e71ef60da2d8ab2410d462ccb7a6513ee64266
Anthony Moss <asmoss93@gmail.com>
2019-08-12 18:47:52 -0400

    adds query selectors for the output data paragraphs and sets content with textContent, form nowworks and shows weather when input is submitted

--- public/js/app.js
+++ public/js/app.js
@@ -3,20 +3,25 @@ console.log('Client side js file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    
     const location = search.value
+    
+    messageOne.textContent = 'Loading...'
 
     fetch(`http://localhost:3001/weather?address=${location}`).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                console.log(data.error)
+        response.json().then(({ error, location, forecast }) => {
+            if (error) {
+                messageOne.textContent = error
             } else {
-                console.log(data.forecast)
-                console.log(data.location)
+                messageOne.textContent = location
+                messageTwo.textContent = forecast.summary
             }
         })
     })
-})
+}) 
