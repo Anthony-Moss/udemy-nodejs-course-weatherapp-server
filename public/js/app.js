0af859ed3f7e5b39968399499440f913a708d6b1
Anthony Moss <asmoss93@gmail.com>
2019-08-12 18:34:33 -0400

    moves fetch inside our submit eventlistener, form is now hooked up but data only displayed inside the console.

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,22 @@
 console.log('Client side js file is loaded!')
 
 
-fetch('http://localhost:3001/weather?address=atlanta').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.forecast)
-            console.log(data.location)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch(`http://localhost:3001/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.forecast)
+                console.log(data.location)
+            }
+        })
     })
 })
