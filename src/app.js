4f91d69b0da1b63d861885f4aca563ea2d8f6cea
Anthony Moss <asmoss93@gmail.com>
2019-08-12 17:35:45 -0400

    adds dummy /products for understanding api endpoints

--- src/app.js
+++ src/app.js
@@ -40,11 +40,28 @@ app.get('/help', (req, res) => {
     })
 });
 
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    res.send({
+        products: []
+    })
+})
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: "You must provide an address"
+        })
+    }
     res.send({
         forecast: 'Sunny',
-        location: 'Atlanta'
+        location: 'Atlanta',
+        address: req.query.address
     })
 });
 
