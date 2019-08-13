0c5a8b196b577571dcde31a87cf4c2ef8be5fdd0
Anthony Moss <asmoss93@gmail.com>
2019-08-12 20:18:18 -0400

    updates file so can use port provided by heroku or 3001 if run locally

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3001
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -77,11 +78,6 @@ app.get('/weather', (req, res) => {
             })
         })
     })
-    // res.send({
-    //     forecast: 'Sunny',
-    //     location: 'Atlanta',
-    //     address: req.query.address
-    // })
 });
 
 app.get('/help/*', (req, res) => {
@@ -100,6 +96,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3001, () => {
-    console.log(`Server is running on port 3001`)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
